9c664fa0644f97a33e4d9d3a7482e11e3439574f
Teza Alfian <tezaalfian2916@gmail.com>
2019-04-11 10:11:12 +0700

    Naon we

---
+++ app/src/main/java/com/example/tezaalfian/koperasipetik/Homepage.java
@@ -0,0 +1,13 @@
+package com.example.tezaalfian.koperasipetik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_homepage);
+    }
+}
